b40fb4da31be87884e23fe1030e0e1dc4b3b98f8
ADAN GIOVANNI RAMIREZ ERIVES <a1124250019@utch.edu.mx>
2024-09-14 16:18:57 -0600

    me gusta el beige

--- scripts/tareas.js
+++ scripts/tareas.js
@@ -1,1 +1,22 @@
-let listaTareas = [];
+let tareas = [];
+
+const formulario = document.getElementById('tarea');
+
+formulario.addEventListener('submit', function(event) {
+    event.preventDefault(); // Evitar recarga de la página
+    
+    const tarea = document.getElementById('task').value;
+
+    tareas.push({ tarea });
+
+    const tareasHTML = document.getElementById('tareas');
+    tareasHTML.innerHTML = '';
+    
+    tareas.forEach((dato) => {
+        const li = document.createElement('li');
+        li.textContent = `${dato.tarea}`;
+        tareasHTML.appendChild(li);
+    });
+    
+    formulario.reset();
+});
